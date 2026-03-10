a063fe4455fb876faac9ed04b96d774d41ef5828
HopeyClarkey <thehopeclarke@gmail.com>
2026-03-10 13:56:29 -0500

    Moodgame component rendering on page.

--- client/components/MoodGame.jsx
+++ client/components/MoodGame.jsx
@@ -1,23 +1,24 @@
 import { useState, useEffect } from 'react';
 
+
 const MoodGame = () => {
   const [initalMouseClick, setInitialMouseClick] = useState(false);
   const [fourSecondMouseChange, setFourSecondMouseChange] = useState(false);
   const [sevenSecondMouseChange, setSevenSecondMouseChange] = useState(false);
   const [eightSecondMouseChange, setEightSecondMouseChange] = useState(false);
 
-  const handleInitialMouseClick = (e) => {
-    setInitialMouseClick(true);
-  };
-  const handleFourSecondMouseChange = (e) => {
-    setFourSecondMouseChange(true);
-  };
-  const handleSevenSecondMouseChange = (e) => {
-    setSevenSecondMouseChange(true);
-  };
-  const handleEightSecondMouseChange = (e) => {
-    setEightSecondMouseChange(true);
-  };
+  // const handleInitialMouseClick = (e) => {
+  //   setInitialMouseClick(true);
+  // };
+  // const handleFourSecondMouseChange = (e) => {
+  //   setFourSecondMouseChange(true);
+  // };
+  // const handleSevenSecondMouseChange = (e) => {
+  //   setSevenSecondMouseChange(true);
+  // };
+  // const handleEightSecondMouseChange = (e) => {
+  //   setEightSecondMouseChange(true);
+  // };
   //   className={isEnterMouse ? 'MouseEnterClass' : ''}
   //   onClick={() => handleInitialMouseClick(true)}
   //   ondrag={() => handleFourSecondMouseChange(true)}
@@ -25,7 +26,7 @@ const MoodGame = () => {
   //   onDrop={() => handleEightSecondMouseChange(true)}
   // >
   return (
-    <div className="wof-component container">
+    <div className="wof-component container" style={{alignContent: 'center'}}>
       <h1 className="text-primary">
         This is where mood game will eventually go!
       </h1>
